e9d8891374bbff4ac43d40eebe7fcd78138f6c72
Stefanibus <stefan.brueck@posteo.de>
2021-06-18 09:51:16 +0200

    established basic Structure & created Header-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import Header from './components/Header'
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      ' Inter ',
+      ' sans-serif ',
+    ].join(','),
+  },
+  palette: {
+    neutral: {
+      main: '#666',
+    },
+    primary: {
+      main: '#ff0000',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
+});
+
+
 
 function App() {
+
   return (
+   <ThemeProvider theme={theme}>
+    <CssBaseline />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xl">
+          <Header/>
+          <div>Searchfield</div>
+          <Checkbox  defaultChecked  />
+          <Checkbox  defaultChecked  />
+          <Checkbox  defaultChecked  />
+          <div>Sort by Name</div>
+          <div>CARDS
+            <Button variant="contained" color="primary">primary To Go</Button>
+            <Button variant="contained" className="nix" color="secondary"> Button</Button>
+
+            <div>(Agent / Property / Shop )-Image Pseudoelement  </div>
+            <div>(Agent / Property / Shop )-Name</div>
+            <div>Adress: </div>
+            <div>Telefone: </div>
+          </div>
+      </Container>
     </div>
+
+   </ThemeProvider>
   );
 }
+
 
 export default App;
